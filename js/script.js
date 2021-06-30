acbf485c9f32b388faca3d740dd7a1cc298f1fc5
Lex <Lexor.V.A.S@yandex.ru>
2021-06-30 23:49:03 +0300

    popup button add

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/**Button **/
+const profileButtonAdd = document.querySelector('.profile__button-add');
+const profileButtonEdit = document.querySelector('.profile__edit-button');
+const popupButtonSave = document.querySelector('.popup__button-save');
+const popupCloseButton = document.querySelector('.popup__button-close');
+const placeButtonHeart = document.querySelectorAll('.place__button-heart');
+/** container **/
+const popupEditProfile = document.querySelector('#popupEditProfile');
+/**input**/
+const newNameProfile = document.querySelector('#newNameProfile');
+const newBusyProfile = document.querySelector('#newBusyProfile');
+/**text **/
+let ProfileName = document.querySelector('#ProfileName');
+let ProfileDescription = document.querySelector('#ProfileDescription');
+/** Event handler **/
+profileButtonEdit.addEventListener('click', function () {
+    popupEditProfile.classList.add('popup_opened');
+});
+popupCloseButton.addEventListener('click' , function(){
+    popupEditProfile.classList.remove('popup_opened');
+});
+popupButtonSave.addEventListener('click',function(){
+    ProfileName.textContent = newNameProfile.value;
+    ProfileDescription.textContent = newBusyProfile.value;
+})
+
+
+
+
+
