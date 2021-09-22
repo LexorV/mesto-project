375ae6cb5ca43e9a2595f42a840464ed44000b6b
Lex <lexor.V.A.S@yandex.ru>
2021-09-22 13:14:33 +0300

    fix modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,19 +1,17 @@
+const handleESC = (form) => (evt) => {
+    if (evt.key === 'Escape') {
+        closePopup(form);
+    };
+}
 export function openPopup(form) {
     form.classList.add('popup_opened');
-    document.addEventListener('keydown', function(evt) {
-        if (evt.key == 'Escape') {
-            closePopup(form);
-        }
-    })
+    document.addEventListener('keydown', handleESC(form));
 };
 export function closePopup(form) {
     form.classList.remove('popup_opened');
-    document.removeEventListener('keydown', function(evt) {
-        if (evt.key == 'Escape') {
-            closePopup(form);
-        }
-    })
-};
+    document.removeEventListener('keydown', handleESC(form));
+}
+
 
 export function setPopupCloseEventListeners() {
     const popapAll = Array.from(document.querySelectorAll('.popup'));
