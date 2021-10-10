5230d3dbab209c188a8da2e2b7ac6f8f4792794a
Lex <Lexor.V.A.S@yandex.ru>
2021-10-10 23:34:52 +0300

    update card part_3

--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,135 @@
 export default class Card {
-    constructor({ data, handleCardClick }, cardSelector) {
-        this._selector = cardSelector;
-    }
-    _getPlaces() {
-        const placesMain = document
-            .querySelector(this._selector)
-            .content
-            .querySelector('.place')
-            .cloneNode(true);
-        return placesMain
+    constructor({ data, user, api, handleCardClick }, cardSelector) {
+        this._handleCardClick = handleCardClick;
+        this._data = data;
+        this._user = user;
+        this._api = api;
+        this._cardSelector = cardSelector;
+
+        this._placesMainTemplate = document.querySelector(cardSelector).content;
+        this._newPlace = this._placesMainTemplate.querySelector('.place').cloneNode(true);
+        this._placePicture = this._newPlace.querySelector('.place__picture');
+        this._removeButton = this._newPlace.querySelector('.place__remove');
+        this._placeName = this._newPlace.querySelector('.place__name');
+        this._buttonHeart = this._newPlace.querySelector('.place__button-heart');
+        this._amountCard = this._newPlace.querySelector('.place__counter-heart');
+
+        this._handleCardClick = this._handleCardClick.bind(this);
+        this._likeCard = this._likeCard.bind(this);
+        this._deleteCard = this._deleteCard.bind(this);
     }
 
 
+
+
+
+    _likeCheck() {
+        const userId = this._user._id;
+        const result = this._data.likes.some((resident) => {
+            return resident._id == this._user._id
+        })
+        return result
+    }
+
+    _renderLikes() {
+        if (checkLike()) {
+            this._buttonHeart.classList.add('place__button-heart_active');
+        }
+    }
+    deleteCard() {
+        this._api.deleteCard(this._data)
+            .then(() => {
+                this._newPlace.remove();
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+
+
+    _setEventListeners() {
+        this._buttonHeart.addEventListener('click', this._renderLikes)
+        this._removeButton.addEventListener('click', this._deleteCard)
+        this._placePicture.addEventListener('click', this._handleCardClick);
+
+        buttonHeart.addEventListener('click', function() {
+            if (!buttonHeart.classList.contains('place__button-heart_active')) {
+                likesAdd(data)
+                    .then((res) => {;
+                        buttonHeart.classList.add('place__button-heart_active');
+                        amountCard.textContent = res.likes.length;
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+            } else {
+                likesRemove(data)
+                    .then((res) => {
+                        buttonHeart.classList.remove('place__button-heart_active');
+                        amountCard.textContent = res.likes.length;
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+            }
+        });
+    }
+
+    // _getPlace() {
+    //     const placesMain = document
+    //         .querySelector(this._cardSelector)
+    //         .content
+    //         .querySelector('.place')
+    //         .cloneNode(true);
+    //     return placesMain
+    // }
+
+    generate() {
+        // this._element = this._getPlace();
+
+        this._placeName.textContent = data.name;
+        this._placePicture.src = data.link;
+        this._placePicture.alt = data.name;
+        this._amountCard.textContent = data.likes.length;
+
+
+        this._setEventListeners();
+
+
+        // this._element.newPlace.querySelector('.place__remove');
+        // this._element.newPlace.querySelector('.place__name').textContent = data.name;
+        // this._element.this._newPlace.querySelector('.place__button-heart');
+        // this._amountCard = this._newPlace.querySelector('.place__counter-heart').textContent =  data.likes.length;
+
+        // this._element.this._newPlace.querySelector('.place__button-heart').addEventListener(;)
+
+        // this._setEventListeners();
+    }
 }
+
+let currUser;
+
+fetch('./user').then((user) => currUser = user)
+
+const card = new Card({ data: { link: "", name: "" }, user: currUser }, "#card");
+
+
+if (card._likeCheck()) {
+    // render active likes
+} else {
+    // render inactive likes
+}
+
+
+
+
+
+
+
+
+
+
 //======================================================
 import { openPopup } from './modal.js';
 import { deleteCard, likesAdd, likesRemove, } from './api.js';
@@ -27,6 +144,7 @@ export function startCards(arrayCard, data) {
 }
 
 import { PopupWithImage } from './Popup.js';
+import { data } from 'autoprefixer';
 
 const popupBigPlace = new PopupWithImage('#popupBigPlace');
 
